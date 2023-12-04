e2989c1cfa0ceca7d05375c83abc5337d3b75145
Yulia Gladysheva <yulliagladysheva@gmail.com>
2023-12-04 16:17:12 +0600

    add btn-scroll to top

---
+++ src/components/scrollToTop/ScrollToTop.tsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+
+const ScrollToTop = () => {
+    const [showTopBtn, setShowTopBtn] = useState(false);
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 40) {
+                setShowTopBtn(true);
+            } else {
+                setShowTopBtn(false);
+            }
+        });
+    }, []);
+    const goToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    };
+    return (
+        <div className="top-to-btm">
+            {" "}
+            {showTopBtn && (
+                <button
+                    type="button"
+                    data-te-ripple-init
+                    data-te-ripple-color="light"
+                    className="!fixed z-20 bottom-5 right-5 rounded-full bg-primary p-3 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-black hover:shadow-lg focus:bg-black focus:shadow-lg focus:outline-none focus:ring-0 active:bg-black active:shadow-lg"
+                    onClick={goToTop}
+                >
+                    <svg
+                        aria-hidden="true"
+                        focusable="false"
+                        data-prefix="fas"
+                        className="h-4 w-4"
+                        role="img"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 448 512"
+                    >
+                        <path
+                            fill="currentColor"
+                            d="M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z"
+                        ></path>
+                    </svg>
+                </button>
+            )}{" "}
+        </div>
+    );
+};
+export default ScrollToTop;
